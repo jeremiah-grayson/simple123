c56d05937187a64e36f0f12c93fd2b0415316038
jeremiah-grayson <grayson4yhwh@gmail.com>
2020-03-29 19:46:37 +0530

    added sub method commit 3

--- simple/src/org/cap/demo/Calculations.java
+++ simple/src/org/cap/demo/Calculations.java
@@ -5,14 +5,22 @@ public class Calculations
 	int a=11;
 	int b=12;
 	int c=a+b;
+	int d=a-b;
 	public void add()
 	{
 		System.out.println(c);
+	}
+	public void sub()
+	{
+		System.out.println(d);
 	}
 	public static void main(String args[])
 	{
 		Calculations cl=new Calculations();
 		cl.add();
 		//new branch is created.
+		Calculations cc=new Calculations();
+		cc.sub();
+		//sub is added.
 	}
 }
